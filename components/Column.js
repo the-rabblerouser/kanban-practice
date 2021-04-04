fe16f585ef27b996c7538e7de08855db3a5dbee7
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-04 12:22:09 -0500

    column and list with task items created

---
+++ components/Column.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Column = () => {
+	return <div>Hello You</div>;
+};
+
+export default Column;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
 import Head from 'next/head';
+import { useState } from 'react';
+
+import { DragDropContext } from 'react-beautiful-dnd';
+
+import initialData from '../utils/initialData';
 import styles from '../styles/Home.module.css';
 
+const Task = ({ tasks }) => {
+	return (
+		<div>
+			<div>
+				{tasks.map(({ content }) => (
+					<div className={styles.container}>{content}</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+const Column = ({ column, tasks }) => {
+	console.log(tasks);
+	return (
+		<div className={styles.container}>
+			<h3 className={styles.title}>{column[0].title}</h3>
+			<div className={styles.taskList}>
+				<Task tasks={tasks} />
+			</div>
+		</div>
+	);
+};
+
 export default function Home() {
+	const [data, SetData] = useState(initialData);
+
+	const { columns, tasks } = data;
+
+	const column = data.columnOrder.map((columnId) => columns[columnId]);
+	const listTasks = column[0].taskIds.map((taskId) => tasks[taskId]);
+
 	return (
 		<div>
 			<Head>
@@ -9,7 +45,9 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<div>Hello World</div>
+			<div>
+				<Column key={column.id} column={column} tasks={listTasks} />
+			</div>
 		</div>
 	);
 }
