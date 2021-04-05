98672fcd116c20eda4dcc2ed8c60c03cc5eb0c46
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-04 22:54:04 -0500

    state is now updated when an item is dragged to a new location

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,42 @@ export default function Home() {
 
 	const { columns, tasks, columnOrder } = data;
 
-	const onDragEnd = () => {};
+	const onDragEnd = (result) => {
+		const { destination, source, draggableId } = result;
+
+		if (!destination) {
+			return;
+		}
+		if (
+			destination.droppableId === source.droppableId &&
+			destination.index === source.index
+		) {
+			return;
+		}
+		const column = data.columns[source.droppableId];
+
+		const newTaskIds = [...column.taskIds];
+
+		// removes item from array
+		newTaskIds.splice(source.index, 1);
+		// starts from destination.index, removes nothing, and adds item to new location
+		newTaskIds.splice(destination.index, 0, draggableId);
+
+		const newColumn = {
+			...column,
+			taskIds: newTaskIds,
+		};
+
+		const newState = {
+			...data,
+			columns: {
+				...columns,
+				[newColumn.id]: newColumn,
+			},
+		};
+
+		setData(newState);
+	};
 
 	return (
 		<>
@@ -33,11 +68,16 @@ export default function Home() {
 	);
 }
 
-// *************************
-// Don't know why, but this code fixes the error:
-// Warning: Prop `data-rbd-draggable-context-id` did not match. Server: "1" Client: "0" div
-// Some issue with react-beautiful-dnd and next.js
-// *************************
+/* 
+The resetServerContext function should be used when server side rendering (SSR). 
+It ensures context state does not persist across multiple renders on the server which would 
+result in client/server markup mismatches after multiple requests are rendered on the server.
+
+Use it before calling the server side render method
+
+ fixes the error:
+ Warning: Prop `data-rbd-draggable-context-id` did not match. Server: "1" Client: "0" div
+*/
 
 export const getServerSideProps = async ({ query }) => {
 	resetServerContext(); // <-- CALL RESET SERVER SIDE
